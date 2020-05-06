0b1fbb935e93e32d644752ea07447a306023c826
Maccbrainy <iketakumichaelnedum@gmail.com>
2020-05-06 13:11:01 +0100

    Initial commits

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme and one of the two most required files for the theme (the other is being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package seedwps
+ */
+
+get_header(); ?>
+
+<div class="container mx-auto">
+	<div class="flex flex-wrap">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+				<?php
+					if( have_posts ()) :
+
+						if (is_home() && ! is_front_page()) :
+				?>
+						<header>
+							<h1 class="page-title">
+								<?php single_post_title();?>
+							</h1>
+						</header>
+				<?php
+						endif;
+
+						/*Start the Loop*/
+
+						while (have_posts()) :
+							the_post();
+
+							get_template_part('views/content',get_post_format());
+						endwhile;
+
+						the_posts_navigation();
+
+					else:
+
+						get_template_part('views/content','none');
+
+					endif;
+
+				?>
+			</main><!-- #main -->
+		</div><!-- .content-area -->
+		<div id="sidebar">
+			<?php get_sidebar(); ?>
+		</div><!-- #sidebar -->
+	</div><!-- .flex -->
+</div><!-- .container -->
+
+<?php get_footer();?>
