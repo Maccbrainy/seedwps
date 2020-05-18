124fe21c4e60b604c07685210cb0b779383d723d
Maccbrainy <iketakumichaelnedum@gmail.com>
2020-05-18 09:03:41 +0100

    Added a css style classes

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 	<?php wp_head();?>
 </head>
 
-<body <?php body_class();?>>
+<body <?php body_class('container');?>>
 	<?php wp_body_open();?>
 
 	<div id="page" class="site">
@@ -36,37 +36,41 @@
 			}
 			?>
 
-			<div class="container mx-auto">
-				<div class="flex flex-wrap">
-					<div class="site-branding">
-						<h1 class="site-title">
-								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-									<?php svg('wordpress');?>
-									<span><?php bloginfo( 'name' ); ?></span>
-								</a>
-							</h1>
+			<div class="container container-fluid">
+				<div class="row">
+					<div class="col-xs-12 col-sm-4">
+						<div class="site-branding">
+							<h1 class="site-title">
+									<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+										<?php svg('wordpress');?>
+										<span><?php bloginfo( 'name' ); ?></span>
+									</a>
+								</h1>
+								<?php
+								$description = get_bloginfo( 'description', 'display' );
+								if ( $description || is_customize_preview() ) :
+								?>
+									<h3 class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></h3>
+								<?php
+								endif;
+								?>
+						</div> <!-- .site-branding -->
+					</div><!-- .col-xs-12 col-sm-4 -->
+					<div class="col-xs-12 col-sm-8">
+						<nav id="site-navigation" class="main-navigation" role="navigation">
 							<?php
-							$description = get_bloginfo( 'description', 'display' );
-							if ( $description || is_customize_preview() ) :
+								if(has_nav_menu('primary')):
+									wp_nav_menu(
+										array(
+											'theme_location' =>'primary',
+											'menu_id' =>'primary-menu',
+											'walker' => new seedwps\Core\WalkerNav()
+										));
+								endif;
 							?>
-								<h3 class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></h3>
-							<?php
-							endif;
-							?>
-					</div> <!-- .site-branding -->
-					<nav id="site-navigation" class="main-navigation" role="navigation">
-						<?php
-							if(has_nav_menu('primary')):
-								wp_nav_menu(
-									array(
-										'theme_location' =>'primary',
-										'menu_id' =>'primary-menu',
-										'walker' => new seedwps\Core\WalkerNav()
-									));
-							endif;
-						?>
-					</nav> <!-- #site-navigation -->
-				</div><!-- .flex -->
+						</nav> <!-- #site-navigation -->
+					</div><!-- .col-xs-12 col-sm-8 -->
+				</div><!-- .row -->
 			</div><!-- .container -->
 		</header><!-- .masthead -->
 		<div id="content" class="site-content">
