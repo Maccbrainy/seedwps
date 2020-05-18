579ceebc2c57d4936b7aff629f1168adf2c8ab0c
Maccbrainy <iketakumichaelnedum@gmail.com>
2020-05-18 09:04:20 +0100

    Added a css style classes

--- index.php
+++ index.php
@@ -13,46 +13,48 @@
 
 get_header(); ?>
 
-<div class="container mx-auto">
-	<div class="flex flex-wrap">
-		<div id="primary" class="content-area">
-			<main id="main" class="site-main" role="main">
-				<?php
-					if( have_posts ()) :
+<div class="container">
+	<div class="row">
+		<div class="col-sm-8">
+			<div id="primary" class="content-area">
+				<main id="main" class="site-main" role="main">
+					<?php
+						if( have_posts ()) :
 
-						if (is_home() && ! is_front_page()) :
-				?>
-						<header>
-							<h1 class="page-title">
-								<?php single_post_title();?>
-							</h1>
-						</header>
-				<?php
+							if (is_home() && ! is_front_page()) :
+					?>
+							<header>
+								<h1 class="page-title">
+									<?php single_post_title();?>
+								</h1>
+							</header>
+					<?php
+							endif;
+
+							/*Start the Loop*/
+
+							while (have_posts()) :
+								the_post();
+
+								get_template_part('views/content',get_post_format());
+							endwhile;
+
+							the_posts_navigation();
+
+						else:
+
+							get_template_part('views/content','none');
+
 						endif;
 
-						/*Start the Loop*/
-
-						while (have_posts()) :
-							the_post();
-
-							get_template_part('views/content',get_post_format());
-						endwhile;
-
-						the_posts_navigation();
-
-					else:
-
-						get_template_part('views/content','none');
-
-					endif;
-
-				?>
-			</main><!-- #main -->
-		</div><!-- .content-area -->
-		<div id="sidebar">
+					?>
+				</main><!-- #main -->
+			</div><!-- .content-area -->
+		</div><!-- .col-sm-8 -->
+		<div id="sidebar" class="col-sm-4">
 			<?php get_sidebar(); ?>
 		</div><!-- #sidebar -->
-	</div><!-- .flex -->
+	</div><!-- .row -->
 </div><!-- .container -->
 
 <?php get_footer();?>
