aa42a655bc0550b8fb79280ebe5ebe32ddc34ea3
Maccbrainy <iketakumichaelnedum@gmail.com>
2020-05-18 09:08:08 +0100

    Updated the css style classes

--- 404.php
+++ 404.php
@@ -7,18 +7,20 @@
 get_header();?>
 
 <div class="container">
-	<div class="flex flex-wrap">
-		<div id="primary" class="content-area">
-		    <main id="main" class="site-main" role="main">
+	<div class="row">
+		<div class="col-sm-8">
+			<div id="primary" class="content-area">
+			    <main id="main" class="site-main" role="main">
 
-		        <header class="page-header">
-		            <h1 class="page-title"><?php _e( 'Oops! Page not Found', 'seedwps' ); ?></h1>
-		        </header>
+			        <header class="page-header">
+			            <h1 class="page-title"><?php _e( 'Oops! Page not Found', 'seedwps' ); ?></h1>
+			        </header>
 
-		    </main><!-- #main -->
-		</div><!-- #primary -->
-		<div><?php get_sidebar();?></div>
-	</div><!-- .flex -->
+			    </main><!-- #main -->
+			</div><!-- #primary -->
+		</div><!-- .col-sm-8 -->
+		<div class="col-sm-4"><?php get_sidebar();?></div><!-- .col-sm-4 -->
+	</div><!-- .row-->
 </div><!-- .container -->
 <?php
 get_footer();
