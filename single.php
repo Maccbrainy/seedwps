c290ec443685fdac5a19d76db51719afd959d7b2
Maccbrainy <iketakumichaelnedum@gmail.com>
2020-05-18 09:05:00 +0100

    Added a css style classes

--- single.php
+++ single.php
@@ -9,32 +9,36 @@
 get_header(); ?>
 
 <div class="container">
-	<div class="flex flex-wrap">
-		<div class="content-area" id="primary">
-			<main id="main" class="site-main" role="main">
-				<?php
-					/*
-					Start the Loop
-					 */
-					while ( have_posts() ):
-						the_post();
+	<div class="row">
+		<div class="col-sm-8">
+			<div class="content-area" id="primary">
+				<main id="main" class="site-main" role="main">
+					<?php
+						/*
+						Start the Loop
+						 */
+						while ( have_posts() ):
+							the_post();
 
-						get_template_part('views/content', get_post_format() );
+							get_template_part('views/content', get_post_format() );
 
-						the_post_navigation();
+							the_post_navigation();
 
-						//if comments are open or we have at least one comment, load up the comment template.
-						
-						if(comments_open() || get_comments_number() ) :
-							comments_template();
-					endif;
+							//if comments are open or we have at least one comment, load up the comment template.
+							
+							if(comments_open() || get_comments_number() ) :
+								comments_template();
+						endif;
 
-				endwhile;
+					endwhile;
 
-				?>
-			</main><!-- #main -->
-		</div><!-- .content-area #primary-->
-		<div><?php get_sidebar();?></div>
-	</div><!-- .flex -->
+					?>
+				</main><!-- #main -->
+			</div><!-- .content-area #primary-->
+		</div><!-- .col-sm-8 -->
+		<div class="col-sm-4">
+			<?php get_sidebar();?>		
+		</div><!-- .col-sm-4 -->
+	</div><!-- .row-->
 </div><!-- .container -->
 <?php get_footer();
