fb31d67081c867cfa3013ba5075de08b8a29be4c
Maccbrainy <iketakumichaelnedum@gmail.com>
2020-05-18 09:06:10 +0100

    Updated the css style classes

--- page.php
+++ page.php
@@ -12,30 +12,34 @@
 get_header(); ?>
 
 <div class="container">
-	<div class="flex flex-wrap">
-		<div class="content-area" id="primary">
-			<main id="main" class="site-main" role="main">
-				<?php
-					/*
-					Start the Loop
-					 */
-					while ( have_posts() ):
-						the_post();
+	<div class="row">
+		<div class="col-sm-8">
+			<div class="content-area" id="primary">
+				<main id="main" class="site-main" role="main">
+					<?php
+						/*
+						Start the Loop
+						 */
+						while ( have_posts() ):
+							the_post();
 
-						get_template_part('views/content', 'page' );
+							get_template_part('views/content', 'page' );
 
 
-						//if comments are open or we have at least one comment, load up the comment template.
-						
-						if(comments_open() || get_comments_number() ) :
-							comments_template();
-					endif;
+							//if comments are open or we have at least one comment, load up the comment template.
+							
+							if(comments_open() || get_comments_number() ) :
+								comments_template();
+						endif;
 
-				endwhile;
+					endwhile;
 
-				?>
-			</main><!-- #main -->
-		</div><!-- .content-area #primary-->
-		<div><?php get_sidebar();?></div>
+					?>
+				</main><!-- #main -->
+			</div><!-- .content-area #primary-->
+		</div><!-- .col-sm-8 -->
+		<div class="col-sm-4">
+			<?php get_sidebar();?>
+		</div>
 	</div><!-- .flex -->
 </div><!-- .container -->
