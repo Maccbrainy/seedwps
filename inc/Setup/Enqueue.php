3c5feb9fa8bf975c64a67aea9194c68346a6ea7f
Maccbrainy <iketakumichaelnedum@gmail.com>
2020-06-03 09:33:15 +0100

    Admin script and admin css enqueue

--- inc/Setup/Enqueue.php
+++ inc/Setup/Enqueue.php
@@ -12,15 +12,55 @@ class Enqueue
 	 */
 	public function register()
 	{
+		add_action('admin_enqueue_scripts', array($this,'admin_Scripts'));
 		add_action('wp_enqueue_scripts', array($this,'enqueue_Scripts'));
 	}
-
-	/**
+		/*	
+		========================
+		ADMIN ENQUEUE FUNCTIONS
+		========================
+	*/
+		/**
 	 * Notice the mix() function in wp_enqueue_...
 	 * It provides the path to a versioned asset by Laravel Mix using querystring-based 
 	 * cache-busting (This means, the file name won't change, but the md5. Look here for 
 	 * more information: https://github.com/JeffreyWay/laravel-mix/issues/920 )
 	 */
+		/**
+		 * Admin enqueue scripts
+		 * @param  $hook
+		 * @return
+		 */
+	public function admin_Scripts($hook)
+	{
+		// dd($hook);
+
+		$enqueue_pages_array = array(
+			'toplevel_page_seedwps_theme',
+			'seedwps_page_cpt_manager',
+			'seedwps_page_css_manager'
+		);
+
+		if (in_array($hook, $enqueue_pages_array)) 
+		{
+			wp_enqueue_style('admin-css', mix('css/admin.css'), array(),'1.0.0', 'all');
+		}
+		
+		if ('toplevel_page_seedwps_theme' == $hook){
+		
+		wp_enqueue_media();
+		
+		wp_enqueue_script('admin-js', mix('js/admin.js'), array(), '1.0.0', true);
+
+		}
+	}
+
+		/*	
+		===========================
+		FRONT-END ENQUEUE FUNCTIONS
+		===========================
+	*/
+
 	public function enqueue_Scripts()
 	{
 		// Deregister the built-in version of jQuery from WordPress
