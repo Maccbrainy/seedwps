39184e7432fde04dd87cd9f4c6d274dab9c78f58
Maccbrainy <iketakumichaelnedum@gmail.com>
2020-06-03 09:41:40 +0100

    template for custom admin area

---
+++ views/admin/index.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template part for displaying a custom Admin area
+ *
+ * @link https://developer.wordpress.org/reference/functions/add_menu_page/
+ *
+ * @package seedwps
+ */
+
+?>
+<div class="wrap">
+	<h1> Seedwps Theme</h1>
+	<?php settings_errors(); ?>
+
+	<ul class="nav nav-tabs">
+		<li class="active"><a href="#tab-1">Manage settings</a></li>
+		<li><a href="#tab-2">Updates</a></li>
+		<li><a href="#tab-3">About</a></li>
+	</ul>
+
+	<div class="tab-content">
+		<div id="tab-1" class="tab-pane active">
+			<form method="post" action="options.php">
+				<?php settings_fields('seedwps_theme_settings');?>
+				<?php do_settings_sections('seedwps_theme');?>
+				<?php submit_button();?>		
+			</form>
+		</div>
+		<div id="tab-2" class="tab-pane">
+			<h3>Updates</h3>
+		</div>
+		<div id="tab-3" class="tab-pane">
+			<h3>About</h3>
+		</div>
+	</div>	
+</div>
